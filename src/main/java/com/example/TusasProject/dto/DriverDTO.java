4354321d8212bf54d9537d4fe0b7deb18dd15b3c
TuBA Çayır <tubacayi4545@gmail.com>
2025-05-03 13:22:43 +0300

    first commit3

--- src/main/java/com/example/TusasProject/dto/DriverDTO.java
+++ src/main/java/com/example/TusasProject/dto/DriverDTO.java
@@ -1,52 +1,17 @@
 package com.example.TusasProject.dto;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 
 public class DriverDTO {
     private String trend;
     private String driver;
-    private float impact;
-    private float uncertainty;
+    private double impact;
+    private double uncertainty;
 
     private Long id;
 
-    public Long getId() {
-        return id;
-    }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-
-    public String getTrend() {
-        return trend;
-    }
-
-    public void setTrend(String trend) {
-        this.trend = trend;
-    }
-
-    public String getDriver() {
-        return driver;
-    }
-
-    public void setDriver(String driver) {
-        this.driver = driver;
-    }
-
-    public double getImpact() {
-        return impact;
-    }
-
-    public void setImpact(float impact) {
-        this.impact = impact;
-    }
-
-    public double getUncertainty() {
-        return uncertainty;
-    }
-
-    public void setUncertainty(float uncertainty) {
-        this.uncertainty = uncertainty;
-    }
-    // Getter ve Setter’lar
 }
