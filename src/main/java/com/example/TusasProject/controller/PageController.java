e5fbeff78cfab5cdb5e13088c6033654eef1c7a1
TuBA Çayır <tubacayi4545@gmail.com>
2025-05-06 19:51:06 +0300

    first commit3

--- src/main/java/com/example/TusasProject/controller/PageController.java
+++ src/main/java/com/example/TusasProject/controller/PageController.java
@@ -1,30 +1,45 @@
 package com.example.TusasProject.controller;
 
 ;
+import com.example.TusasProject.entity.User;
+import com.example.TusasProject.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.security.Principal;
+import java.util.Optional;
 
 
 @Controller
 public class PageController {
+
+    @Autowired
+    UserRepository userRepository;
 
     @GetMapping("/")
     public String home() {
         return "home";
     }
 
-  @GetMapping("/panel")
-  public String showPanelPage() {
-    return "panel"; // src/main/resources/templates/panel.html
-  }
+    @GetMapping("/panel")
+    public String panelPage(Model model, Principal principal) {
+        User user = userRepository.findByEmail(principal.getName()).orElse(null);
+
+        if (user != null) {
+            model.addAttribute("userName", user.getFirstName() + " " + user.getLastName());
+            model.addAttribute("userRole", user.getRole().getName());
+            model.addAttribute("userExpertise", user.getExpertise());
+        }
+
+        return "panel"; // src/main/resources/templates/panel.html
+    }
+
     @GetMapping("/driver")
     public  String showDriverPage() {
         return "driver";
     }
-
-
-
-
 
 
 }
